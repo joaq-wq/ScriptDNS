763358e03a6aea1da79491f6d972d35cec6b723f
joaq-wq <joaquimtrello97@gmail.com>
2025-05-21 14:35:43 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -50,15 +50,8 @@ zone "192.in-addr.arpa" {
 };
 EOF
 
-        echo "Copiando arquivos de zona padrão..."
-        echo $SENHA | sudo -S cp /etc/bind/db.local /etc/bind/db.grau.local
-        echo $SENHA | sudo -S cp /etc/bind/db.127 /etc/bind/db.192
-
         echo "Editando arquivo db.grau.local..."
         echo $SENHA | sudo -S tee /etc/bind/db.grau.local > /dev/null <<EOF
-;
-; BIND data file for local loopback interface
-;
 \$TTL    604800
 @       IN      SOA     grau.local. root.grau.local. (
                               2         ; Serial
@@ -67,7 +60,6 @@ EOF
                               2419200   ; Expire
                               604800 )  ; Negative Cache TTL
 
-;
 @       IN      NS      grau.local.
 @       IN      A       192.168.0.1
 www     IN      A       192.168.0.1
@@ -76,9 +68,6 @@ EOF
 
         echo "Editando arquivo db.192..."
         echo $SENHA | sudo -S tee /etc/bind/db.192 > /dev/null <<EOF
-;
-; BIND data file for local loopback interface
-;
 \$TTL    604800
 @       IN      SOA     grau.local. root.grau.local. (
                               2         ; Serial
@@ -87,11 +76,10 @@ EOF
                               2419200   ; Expire
                               604800 )  ; Negative Cache TTL
 
-;
 @       IN      NS      grau.local.
-@       IN      A       192.168.0.1
-www     IN      A       192.168.0.1
-ftp     IN      A       192.168.0.1
+1       IN      PTR     grau.local.
+1       IN      PTR     www.grau.local.
+1       IN      PTR     ftp.grau.local.
 EOF
 
         echo "Reiniciando o serviço BIND9..."
@@ -99,11 +87,8 @@ EOF
 
         echo "Substituindo o arquivo resolv.conf..."
         echo $SENHA | sudo -S mv /etc/resolv.conf /etc/resolv.conf.bkp
-        echo $SENHA | sudo -S touch /etc/resolv.conf
         echo $SENHA | sudo -S tee /etc/resolv.conf > /dev/null <<EOF
 nameserver 192.168.0.1
-www.grau.local 192.168.0.1
-ftp.grau.local 192.168.0.1
 EOF
 
         echo "Configuração concluída com sucesso!"
@@ -120,10 +105,10 @@ EOF
 
         echo "Atualizando pacotes..."
         echo $SENHA | sudo -S zypper refresh
-        echo $SENHA | sudo -S zypper update -y
+        echo $SENHA | sudo -S zypper update --no-confirm
 
-        echo "Instalando BIND9..."
-        echo $SENHA | sudo -S zypper install bind -y
+        echo "Instalando BIND..."
+        echo $SENHA | sudo -S zypper install --no-confirm bind
 
         echo "Configurando IP fixo 192.168.0.1/24 na interface $INTERFACE..."
         echo $SENHA | sudo -S ip addr flush dev $INTERFACE
@@ -131,30 +116,23 @@ EOF
         echo $SENHA | sudo -S ip link set $INTERFACE up
 
         echo "Configurando zonas no named.conf..."
-        echo $SENHA | sudo -S tee /etc/named/named.conf > /dev/null <<EOF
-// Zona Direta
-zone "grau.local" {
+        echo $SENHA | sudo -S tee /etc/named.conf > /dev/null <<EOF
+include "/etc/named.conf.include";
+
+zone "grau.local" IN {
     type master;
-    file "/var/lib/named/db.grau.local";
+    file "/var/lib/named/grau.local.conf";
 };
 
-// Zona Reversa
-zone "192.in-addr.arpa" {
+zone "192.in-addr.arpa" IN {
     type master;
     file "/var/lib/named/db.192";
 };
 EOF
 
-        echo "Copiando arquivos de zona padrão..."
-        echo $SENHA | sudo -S cp /var/lib/named/etc/named.local /var/lib/named/db.grau.local
-        echo $SENHA | sudo -S cp /var/lib/named/etc/named.127 /var/lib/named/db.192
-
-        echo "Editando arquivo db.grau.local..."
-        echo $SENHA | sudo -S tee /var/lib/named/db.grau.local > /dev/null <<EOF
-;
-; BIND data file for local loopback interface
-;
-\$TTL    604800
+        echo "Criando arquivo grau.local.conf..."
+        echo $SENHA | sudo -S tee /var/lib/named/grau.local.conf > /dev/null <<EOF
+\$TTL 604800
 @       IN      SOA     grau.local. root.grau.local. (
                               2         ; Serial
                               604800    ; Refresh
@@ -162,19 +140,15 @@ EOF
                               2419200   ; Expire
                               604800 )  ; Negative Cache TTL
 
-;
 @       IN      NS      grau.local.
 @       IN      A       192.168.0.1
 www     IN      A       192.168.0.1
 ftp     IN      A       192.168.0.1
 EOF
 
-        echo "Editando arquivo db.192..."
+        echo "Criando arquivo db.192..."
         echo $SENHA | sudo -S tee /var/lib/named/db.192 > /dev/null <<EOF
-;
-; BIND data file for local loopback interface
-;
-\$TTL    604800
+\$TTL 604800
 @       IN      SOA     grau.local. root.grau.local. (
                               2         ; Serial
                               604800    ; Refresh
@@ -182,23 +156,20 @@ EOF
                               2419200   ; Expire
                               604800 )  ; Negative Cache TTL
 
-;
 @       IN      NS      grau.local.
-@       IN      A       192.168.0.1
-www     IN      A       192.168.0.1
-ftp     IN      A       192.168.0.1
+1       IN      PTR     grau.local.
+1       IN      PTR     www.grau.local.
+1       IN      PTR     ftp.grau.local.
 EOF
 
-        echo "Reiniciando o serviço BIND9..."
+        echo "Habilitando e reiniciando o serviço named..."
+        echo $SENHA | sudo -S systemctl enable named
         echo $SENHA | sudo -S systemctl restart named
 
         echo "Substituindo o arquivo resolv.conf..."
         echo $SENHA | sudo -S mv /etc/resolv.conf /etc/resolv.conf.bkp
-        echo $SENHA | sudo -S touch /etc/resolv.conf
         echo $SENHA | sudo -S tee /etc/resolv.conf > /dev/null <<EOF
 nameserver 192.168.0.1
-www.grau.local 192.168.0.1
-ftp.grau.local 192.168.0.1
 EOF
 
         echo "Configuração concluída com sucesso!"
